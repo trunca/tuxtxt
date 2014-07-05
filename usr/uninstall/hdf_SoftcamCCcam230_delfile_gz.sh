463364da6e24a83117a3c5ccc91e2982e29356b5
SFTEAM <truncalinos@gmail.com>
2014-07-05 20:14:10 +0200

    tuxtxt

---
+++ usr/uninstall/hdf_SoftcamCCcam230_delfile_gz.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+rm -f /etc/init.d/softcam.CCcam-230 > /dev/null 2>&1
+rm -f /etc/init.d/cardserver.CCcam-230 > /dev/null 2>&1
+rm -f /usr/bin/CCcam230 > /dev/null 2>&1
+rm -f /usr/uninstall/hdf_SoftcamCCcam230_delfile_gz.sh
+exit 0
